48aa0a7e2524f4fc9aa561cab16f8c627afc0ecc
Andika Dinata <a.notmuehehe@gmail.com>
2026-07-03 20:52:04 +0700

    style: fix play/pause icon alignment

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -250,9 +250,9 @@ export default function Home() {
                    }
                 }} className="w-14 h-14 flex items-center justify-center bg-white text-black hover:bg-gray-200 shadow-xl rounded-full active:scale-95 transition-all mx-2">
                   {isPlaying ? (
-                    <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" /></svg>
+                    <svg className="w-7 h-7" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M6 4h4v16H6zm8 0h4v16h-4z" /></svg>
                   ) : (
-                    <svg className="w-6 h-6 ml-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" /></svg>
+                    <svg className="w-7 h-7 ml-1" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M8 5v14l11-7z" /></svg>
                   )}
                 </button>
                 
